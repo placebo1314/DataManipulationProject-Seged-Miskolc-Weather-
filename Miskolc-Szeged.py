888f0829669638e1e41bfae28f1d2ff9f05424e2
Krisz <placebo1314@gmail.com>
2023-02-11 12:23:53 +0100

    Implement plots

--- Miskolc-Szeged.py
+++ Miskolc-Szeged.py
@@ -1,21 +1,88 @@
+import calendar
 import numpy as np
 import pandas as pd
+pd.options.mode.chained_assignment = None
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 def analize_data(miskolc, szeged):
+    # # Resample the data to calculate average temperature per quarter year
+    # miskolc_quarterly = miskolc.resample('Q', on='Date').mean()
+    # szeged_quarterly = szeged.resample('Q', on='Date').mean()
+    #mpl.style.use('dark_background')
+    mpl.style.use('seaborn-darkgrid')
+    # plt.plot(miskolc_quarterly.index, miskolc_quarterly.Average_temperature_C, label = 'Miskolc', color = 'red', 
+    # linewidth = 2, marker = 'o', markersize = 6, markeredgecolor = 'black')
+    # plt.plot(szeged_quarterly.index, szeged_quarterly.Average_temperature_C, label = 'Szeged', 
+    # color = 'blue', linewidth = 2, marker = 's', markersize = 6, markeredgecolor = 'black')
 
-    #compare Miskolc-Szeged avg temps
-    plt.figure(figsize=(20, 8))
+    miskolc_dates = miskolc.Date[miskolc.Date.dt.month % 2 == 0]
+    miskolc_temps = miskolc.Average_temperature_C[miskolc.Date.dt.month % 2 == 0]
 
-    plt.plot(miskolc.Date, miskolc.Average_temperature_C, label='Miskolc')
+    szeged_dates = szeged.Date[szeged.Date.dt.month % 2 == 0]
+    szeged_temps = szeged.Average_temperature_C[szeged.Date.dt.month % 2 == 0]
 
-    plt.plot(szeged.Date, szeged.Average_temperature_C, label='Szeged')
+    plt.plot(miskolc_dates, miskolc_temps, label = 'Miskolc', color = 'red', linewidth = 2, marker = 'o', markersize = 6, markeredgecolor = 'black')
+    plt.plot(szeged_dates, szeged_temps, label = 'Szeged', color = 'blue', linewidth = 2, marker = 's', markersize = 6, markeredgecolor = 'black')
 
-    plt.legend(loc='best', framealpha=0.5)
-
+    plt.legend(loc='upper right', bbox_to_anchor=(1.15, 1.05), framealpha=0.5, fontsize=12)
     plt.ylabel('Avg. Temperature')
+    plt.suptitle('Compare Avg. Temperature (2017-2021)')
+    plt.savefig('Miskolc_Szeged_Avg_temp_(2017-2021)plot.png')
+    #plt.show()
+    clearPlts()
 
-    plt.show()
+
+    mpl.style.use('seaborn-darkgrid')
+    month_names = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+    plt.plot(miskolc.groupby(miskolc.Date.dt.strftime('%b'))['Rainy_days'].mean(), label='Miskolc', color='red', linewidth=2, marker='o', markersize=8, markeredgecolor='black')
+    plt.plot(szeged.groupby(szeged.Date.dt.strftime('%b'))['Rainy_days'].mean(), label='Szeged', color='blue', linewidth=2, marker='s', markersize=8, markeredgecolor='black')
+
+    plt.ylabel('Avg. Rainy days', fontsize=14)
+    plt.xlabel('Month', fontsize=14)
+
+    plt.xticks(range(12), month_names, fontsize=12, rotation=45)
+    plt.yticks(fontsize=12)
+
+    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), framealpha=0.5, fontsize=12)
+    plt.suptitle('Compare Avg. rainy days (2017-2021)', fontsize=16)
+
+    plt.savefig('Miskolc_Szeged_Avg_rainy_days_(2017-2021)plot.png', dpi=150, bbox_inches='tight')
+    #plt.show()
+    clearPlts()
+
+
+    fig = plt.figure(figsize=(15, 10))
+    ax1 = plt.subplot2grid((1, 2), (0, 0))
+    ax2 = plt.subplot2grid((1, 2), (0, 1))
+    labels = calendar.month_name[1:]
+    autopct = '%1.1f%%'
+    values1 = miskolc.groupby(miskolc.Date.dt.month)['Precipitation_mm'].mean()
+    values2 = szeged.groupby(szeged.Date.dt.month)['Precipitation_mm'].mean()
+    colors = ['blue', 'green', 'red', 'cyan', 'magenta', 'yellow', 'gray', 'pink', 'silver', 'gold', 'brown', 'purple']
+
+    ax1.pie(values1, labels = labels,
+               autopct = autopct, explode = [0.05]*12,
+               shadow = True, startangle = 90, counterclock = False, colors = colors, pctdistance = 0.85)
+    ax1.set_title('Monthly Average Precipitation in Miskolc', fontsize = 14, fontweight = 'bold')
+    centre_circle = plt.Circle((0,0),0.7,fc = 'white')
+    ax1.add_artist(centre_circle)
+    rain_sum = miskolc.groupby(miskolc.Date.dt.month)['Precipitation_mm'].mean().sum()
+    ax1.text(0, 0, f"Avg. rain per year: {rain_sum:.1f} mm", ha="center", va="center", fontweight = 'bold')
+
+    ax2.pie(values2, labels = labels,
+               autopct = autopct, explode=[0.05]*12,
+               shadow = True, startangle = 90, counterclock = False, colors = colors, pctdistance = 0.85)
+    ax2.set_title('Monthly Average Precipitation in Szeged', fontsize = 14, fontweight = 'bold')
+    centre_circle = plt.Circle((0,0),0.7,fc = 'white')
+    ax2.add_artist(centre_circle)
+    rain_sum = szeged.groupby(szeged.Date.dt.month)['Precipitation_mm'].mean().sum()
+    ax2.text(0, 0, f"Avg. rain per year: {rain_sum:.1f} mm", ha="center", va="center", fontweight = 'bold')
+    ax1.set_aspect(0.85)
+    ax2.set_aspect(0.85)
+    plt.savefig('Miskolc_Szeged_Precipitation(2017-2021)plot.png')
+    #plt.show()
     
 
 def optimize_data(data):
@@ -58,6 +125,7 @@ def optimize_data(data):
     data['Min_temperature_C'] = data['Min_temperature_C'].apply(lambda x: float(x.replace(',', '.')))
     data['Sunny_hours'] = data['Sunny_hours'].apply(lambda x: float(x.split()[0].replace(',', '.')))
     data['Date'] = pd.to_datetime(data['Date'])
+    data['Precipitation_mm'] = data['Precipitation_mm'].apply(lambda x: int(x))
 
     #reorder & delete unnecessary columns:
     data = data[['Date', 'Average_temperature_C', 'Max_temperature_C', 'Min_temperature_C', 'Sunny_hours', 'Rainy_days', 
@@ -65,6 +133,10 @@ def optimize_data(data):
 
     return data
 
+
+def clearPlts():
+    plt.clf()
+    plt.close("all")
 
 def main():
     miskolc = optimize_data(pd.read_csv("stadat-kor0075-15.9.2.6-hu.csv", encoding = 'unicode_escape', header = 1 ,sep=";"))
